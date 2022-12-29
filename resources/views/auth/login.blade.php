220739b8f7fa4fa825d26ed6e0bc61270509e4ea
Mike Stephen Gollena <55428756+DJmistMAGO@users.noreply.github.com>
2022-12-29 10:17:50 +0800

    add logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,4 +2,6 @@
 
 @section('content')
     <h1>Login dd tuay</h1>
+    <h4>tas didi an logo</h4>
+    <img src="{{ asset('images/st_trese.png') }}" alt="" width="300">
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{ asset('images/st_trese.png') }}" type="image/x-icon">
     {{-- lagay ka link --}}
-    <title>Document</title>
+    <title>STHC</title>
 </head>
 
 <body>
