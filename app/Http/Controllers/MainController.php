3230cfa93705e8e45cfdc65572fac4995abe8310
Mike Stephen Gollena <55428756+DJmistMAGO@users.noreply.github.com>
2023-01-03 19:35:29 +0800

    edit authentication

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Household;
-use App\Models\Incident;
-use App\Models\Complaint;
-use Termwind\Components\Dd;
+// use App\Models\Incident;
+// use App\Models\Complaint;
+// use Termwind\Components\Dd;
 
 class MainController extends Controller
 {
@@ -16,11 +16,11 @@ class MainController extends Controller
         // $incidents = Incident::all()->count();
         // $complaints = Complaint::all()->count();
 
-        return view('modules.dashboard', compact('households', 'incidents', 'complaints'));
+        return view('dashboard', compact('households'));
     }
 
-    public function brgyInfo()
-    {
-        return view('brgyinfo.index');
-    }
+    // public function brgyInfo()
+    // {
+    //     return view('brgyinfo.index');
+    // }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,11 +7,12 @@
 @section('body')
     <div class="d-flex flex-column flex-root">
         <div class="login login-5 login-signin-on d-flex flex-row-fluid" id="kt_login">
-            <div class="d-flex flex-center flex-row-fluid bgi-size-cover bgi-position-top bgi-no-repeat" style="background-image: url('{{ asset('assets/media/bg/bg-5.jpg') }}');">
+            <div class="d-flex flex-center flex-row-fluid bgi-size-cover bgi-position-top bgi-no-repeat"
+                style="background-image: url('{{ asset('assets/media/bg/bg-5.jpg') }}');">
                 <div class="login-form text-center p-7 position-relative overflow-hidden">
                     <div class="d-flex flex-center mb-15">
                         <a href="#">
-                            <img src="{{asset('assets/images/st_trese.png')}}" class="max-h-200px" alt=""/>
+                            <img src="{{ asset('assets/images/st_trese.png') }}" class="max-h-200px" alt="" />
                         </a>
                     </div>
                     <div class="login-signin">
@@ -20,39 +21,50 @@
                             <div class="text-muted font-weight-bold">Enter your details to login to your account:</div>
                         </div>
                         {{-- <x-errors/> --}}
-                        <form action="" class="form" method="">
+                        <form action="{{ route('auth.store') }}" class="form" method="post">
+                            @csrf
                             <div class="form-group mb-5">
-                                <input class="form-control h-auto form-control-solid py-4 px-8" type="text" placeholder="Username" name="username" autocomplete="off" autofocus/>
+                                <input
+                                    class="form-control h-auto form-control-solid py-4 px-8 @error('user_name') is-invalid @enderror"
+                                    type="text" placeholder="Username" name="user_name" autocomplete="off" autofocus />
+                                @error('user_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="form-group mb-5">
-                                <input class="form-control h-auto form-control-solid py-4 px-8" type="password" placeholder="Password" name="password"/>
+                                <input
+                                    class="form-control h-auto form-control-solid py-4 px-8 @error('password') is-invalid @enderror"
+                                    type="password" placeholder="Password" name="password" />
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
-                            <div class="form-group d-flex flex-wrap justify-content-between align-items-center">
-                                <div class="checkbox-inline">
-                                    <label class="checkbox m-0 text-muted">
-                                        <input type="checkbox" name="remember"/>
-                                        <span></span>Remember me</label>
-                                </div>
-                                <a href="javascript:;" id="kt_login_forgot" class="text-muted text-hover-primary">Forget Password ?</a>
-                            </div>
+
                             <button class="btn btn-primary font-weight-bold px-9 py-4 my-3 mx-4">Sign In</button>
                         </form>
                     </div>
-                    <div class="login-forgot">
+                    {{-- <div class="login-forgot">
                         <div class="mb-20">
                             <h3>Forgotten Password ?</h3>
                             <div class="text-muted font-weight-bold">Enter your email to reset your password</div>
                         </div>
                         <form class="form" id="kt_login_forgot_form">
                             <div class="form-group mb-10">
-                                <input class="form-control form-control-solid h-auto py-4 px-8" type="text" placeholder="Email" name="email" autocomplete="off"/>
+                                <input class="form-control form-control-solid h-auto py-4 px-8" type="text"
+                                    placeholder="Email" name="email" autocomplete="off" />
                             </div>
                             <div class="form-group d-flex flex-wrap flex-center mt-10">
-                                <button id="kt_login_forgot_submit" class="btn btn-primary font-weight-bold px-9 py-4 my-3 mx-2">Request</button>
-                                <button id="kt_login_forgot_cancel" class="btn btn-light-primary font-weight-bold px-9 py-4 my-3 mx-2">Cancel</button>
+                                <button id="kt_login_forgot_submit"
+                                    class="btn btn-primary font-weight-bold px-9 py-4 my-3 mx-2">Request</button>
+                                <button id="kt_login_forgot_cancel"
+                                    class="btn btn-light-primary font-weight-bold px-9 py-4 my-3 mx-2">Cancel</button>
                             </div>
                         </form>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
