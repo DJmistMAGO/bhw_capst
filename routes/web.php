4d73aa41dc78bb01251262ee323affe8c9ec7308
jkenneth24 <kengerero@yahoo.com>
2023-01-04 18:23:57 +0800

    web chnages

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
-use App\Http\Controllers\ResidentController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\HouseholdController;
 use App\Http\Controllers\ResidentController;
